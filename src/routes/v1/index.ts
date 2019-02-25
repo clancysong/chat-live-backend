601687c9e49a46d5e2670dc79a4662a692590794
clancysong <clancysong@gmail.com>
2019-02-25 17:53:18 +0800

    Add authentication middleware

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,21 +2,19 @@ import Router from 'koa-router'
 import authRouter from './auth'
 import userRouter from './user'
 import response from '../../utils/response'
+import session from '../../utils/session'
 
 const router = new Router({ prefix: '/v1' })
 
-// router.use('/*', (ctx, next) => {
-//   const isPathAllowed = () => {
-//     const allowedPath = [
-//       { path: '/api/v1/session', method: 'POST' },
-//       { path: '/api/v1/user', method: 'POST' }
-//     ]
-//     return allowedPath.some((item => item.path === ctx.path && item.method === ctx.method))
-//   }
+router.use('/*', async (ctx, next) => {
+  const isPathAllowed = () => {
+    const allowedPaths = ['/api/v1/auth/login', '/api/v1/auth/register']
+    return allowedPaths.some(path => ctx.path === path)
+  }
 
-//   if (ctx.isAuthenticated() || isPathAllowed()) next()
-//   else response.error(ctx, 'Authentication failed', 401)
-// })
+  if (session.isAuthenticated(ctx) || isPathAllowed()) await next()
+  else response.error(ctx, 'Authentication failed', 401)
+})
 
 router.use(authRouter.routes())
 router.use(userRouter.routes())
--- src/utils/session.ts
+++ src/utils/session.ts
@@ -16,5 +16,8 @@ export default {
     } else {
       return undefined
     }
+  },
+  isAuthenticated(ctx: Context) {
+    return ctx.session.user ? true : false
   }
 }
